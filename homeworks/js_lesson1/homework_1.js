fce6b9bdbc82a64d677501bbb148923e3986a0cd
demeshonoktetiana-bit <demeshonoktetiana@gmail.com>
2026-05-31 11:31:39 +0300

    виконала перше завдання JS

---
+++ homeworks/js_lesson1/homework_1.js
@@ -0,0 +1,58 @@
+let str1 = 'hello';
+console. log(str1);
+let str2 = 'owu';
+console. log(str2);
+let str3 = 'com';
+console. log(str3);
+let str4 = 'ua';
+console. log(str4);
+
+let num1 = 1;
+console. log(num1);
+let num2 = 10;
+console. log(num2);
+let num3 = -999;
+console. log(num3);
+let num4 = 123;
+console. log(num4);
+let num5 = 3.14;
+console. log(num5);
+let num6 = 2.7;
+console. log(num6);
+let num7 = 16;
+console. log(num7);
+
+let bool1 = true;
+console. log(bool1);
+let bool2 = false;
+console. log(bool2);
+
+
+let firstName = 'Tetiana '
+console. log(firstName);
+let middleName = 'Romanivna '
+console. log(middleName);
+let lastName = 'Demeshonok'
+console. log(lastName);
+let person= firstName + middleName + lastName;
+console. log(person);
+
+
+
+let a = 100;
+console. log(typeof a);
+let b = '100';
+console. log(typeof b);
+let c = true;
+console. log(typeof c);
+
+let name = prompt('enter your name');
+console. log(name);
+let middlename = prompt('enter your middlename');
+console. log(middlename);
+let age = prompt('enter your age');
+console. log(age);
+
+
+
+
